8626321d71ff4cce33555421eaa29ad4f05fd825
ubanand36 <ak0591640@gmail.com>
2025-10-17 09:38:14 +0530

    Added Personality tester Programm in html

--- Recursion/Fibonacci_Series.py
+++
@@ -1,8 +0,0 @@
-def fib(n):
-    if n==1 or n==2:
-        return 1
-    output = fib(n-1) + fib(n-2)
-    return output
-
-n=6
-print(fib(n))
--- Recursion/Sum_of_n.py
+++
@@ -1,7 +0,0 @@
-def sum_n(n):
-    if n==0:
-        return 0
-    return n + sum_n(n-1)
-
-n = 5
-print(sum_n(n))
